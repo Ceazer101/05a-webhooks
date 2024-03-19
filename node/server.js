0620d40b59dee226af77b2603e59be90ac20b413
chris <chri402s@stud.kea.dk>
2024-03-19 16:57:45 +0100

    test webhook

--- node/server.js
+++ node/server.js
@@ -65,4 +65,5 @@ app.post('/webhook', (req, res) => {
     console.log('Received webhook payload:', payload);
     // Here you can handle the payload received from GitHub
     res.status(200).send('Webhook received successfully');
+    console.log(payload);
 });
